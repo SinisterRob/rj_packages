5a097e6f75856bdf1ac20b2f3aaa994678c2b787
SinisterRob <59403610+SinisterRob@users.noreply.github.com>
2022-01-14 23:50:52 -0600

    Add 'df' parameter to write.table function.

--- R/write_files.R.R
+++ R/write_files.R.R
@@ -1,11 +1,11 @@
 #' Write table to clipboard
-#' 
+#'
 #' This function writes a data frame to a format that can be easily pasted into Excel.
-#' 
+#'
 #' @param df Data frame that you want to write to the clipboard.
 #' @return No return: Data frame is in the clipboard.
 #' @export
 
 write_table_rj <- function(df){
-  write.table("clipboard-500000", sep = "\t", row.names = F)
+  write.table(df, "clipboard-500000", sep = "\t", row.names = F)
 }
